586c73e0f28de5817a0239814efc1554fbc18699
Soumik0811 <91133940+Soumik0811@users.noreply.github.com>
2024-09-08 20:44:47 +0530

    Add files via upload

---
+++ src/components/new.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Chatbot = () => {
+    // Your chatbot component code goes here
+
+    return (
+        <div>
+            <input type="text" placeholder="Enter your message" />
+            <button>Send</button>
+        </div>
+    );
+};
+
+export default Chatbot;
